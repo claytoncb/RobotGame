287a10977c6dfa8cffe081a312d65c80f9b48bd4
claytoncb <cbruce4@lion.lmu.edu>
2023-05-09 21:14:35 -0700

    coool!

--- src/Entities/RobotBoy/RobotBoy.py
+++ src/Entities/RobotBoy/RobotBoy.py
@@ -7,39 +7,48 @@ import numpy as np
 
 class RobotBoy(pygame.sprite.Sprite):
     def calculateContibutions(self):
-        self.lightContributionsRight = LightingGenerator.lightContributionFromNormals(self.imageNormalsRight[self.z%len(self.keyframes)] ,LightingGenerator.getLightingVecXZ(self.t))
-        self.lightContributionsLeft = LightingGenerator.lightContributionFromNormals(self.imageNormalsLeft[self.z%len(self.keyframes)] ,LightingGenerator.getLightingVecXZ(self.t)) 
+        if self.movingLeft:
+            self.lightContributions = LightingGenerator.lightContributionFromNormals(self.imageNormalsLeft[self.z%len(self.keyframes)] ,LightingGenerator.getLightingVecXZ(self.t)) 
+            self.colors = self.imageColorsLeft[self.z%len(self.keyframes)]
+        if self.movingRight:
+            self.lightContributions = LightingGenerator.lightContributionFromNormals(self.imageNormalsRight[self.z%len(self.keyframes)] ,LightingGenerator.getLightingVecXZ(self.t))
+            self.colors = self.imageColorsRight[self.z%len(self.keyframes)]
+
+        
     def update(self,dt):
         
         self.nextTime(dt)
-        self.calculateContibutions()
+        
         self.input(dt)
+        self.calculateContibutions()
+
+        
 
         
         if not self.image or UPDATE:
-            
-            image = LightingGenerator.getLitty(self.imageColors[self.z%len(self.keyframes)],LightingGenerator.getLightingVecXZ(self.t),self.lightContributions).resize((self.scale,self.scale), resample=Image.Resampling.NEAREST )
+            image = LightingGenerator.getLitty(self.colors,LightingGenerator.getLightingVecXZ(self.t),self.lightContributions).resize((self.scale,self.scale), resample=Image.Resampling.NEAREST )
             self.image = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
-    def nextAnimation(self,dt):
-        self.z+=1
+            
     def nextTime(self,dt):
         self.t+=dt
     def input (self,dt):
         keys = pygame.key.get_pressed()
         if keys[K_LEFT]:
-            if BUTTON_FEEDBACK: print('left')  
-            self.imageColors = self.imageColorsLeft
-            self.lightContributions = self.lightContributionsLeft
+            if BUTTON_FEEDBACK: print('left') 
+            self.movingLeft = True  
+        else:
+            self.movingLeft = False 
         if keys[K_RIGHT]:
             if BUTTON_FEEDBACK: print('right') 
-            self.imageColors = self.imageColorsRight
-            self.lightContributions = self.lightContributionsRight
+            self.movingRight = True
+        else:
+            self.movingRight = False
         if keys[K_UP]:
             if BUTTON_FEEDBACK: print('up')  
         if keys[K_DOWN]:
             if BUTTON_FEEDBACK: print('down')
         if keys[K_LEFT] or keys[K_RIGHT] or keys[K_UP] or keys[K_DOWN]:
-            self.nextAnimation(dt)
+            self.z += 1
         else:
             self.z = 0
 
@@ -54,19 +63,19 @@ class RobotBoy(pygame.sprite.Sprite):
         self.imageColorsRight = [ Image.open(f"src\\Entities\\RobotBoy\\Textures\\robotBoyColorRight{i}.png") for i in self.keyframes ]
         self.imageNormalsRight = [np.array(imageNormal.getdata()).reshape(imageNormal.size[0], imageNormal.size[1], 4) for imageNormal in[ Image.open(f"src\\Entities\\RobotBoy\\Textures\\robotBoyNormalsRight{i}.png") for i in self.keyframes ]]
         self.t = 0
-        self.z = 1
-        
-        self.calculateContibutions()
-        self.lightContributions = self.lightContributionsLeft
-        self.imageColors = self.imageColorsLeft
-        self.imageNormals= self.imageNormalsLeft
-        
-        self.scale=scale
-        
-        self.movingLeft = False
+        self.z = 0
+
+        self.movingLeft = True
         self.movingRight = False
         self.movingUp = False
         self.movingDown = False
+        
+        self.calculateContibutions()
+        self.imageColors = self.imageColorsLeft
+        
+        self.scale=scale
+        
+        
         self.t = math.pi*12
         self.x = pos_x
         self.y = pos_y

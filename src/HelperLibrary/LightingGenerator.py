123af22d0aa77058a9c2c2e506aa7a2774b5316f
claytoncb <cbruce4@lion.lmu.edu>
2023-06-18 10:20:06 -0700

    basic highlights concept

--- src/HelperLibrary/LightingGenerator.py
+++ src/HelperLibrary/LightingGenerator.py
@@ -5,7 +5,8 @@ from numpy.linalg import norm
 import pygame
 from settings import *
 
-
+def magnitude(vector):
+    return np.sqrt(np.sum(np.power(vector,2),axis=1))
 
 def getLitty(normals, colors, size,lightingVec):
     lightingVec = lightingVec/norm(lightingVec)
@@ -26,11 +27,13 @@ def getLitty(normals, colors, size,lightingVec):
     
 
     # Compute the dot product between newNormals and lightingVec
-    norm_dot_product = ((np.dot(newNormals.reshape((size[0]*size[1],3)), lightingVec)+1)/2).reshape((size[0]*size[1]))/128
+    norm_dot_product = ((np.dot(newNormals, lightingVec)+1)/2).reshape((size[0]*size[1]))/128
+    
+    
 
     #apply sigmoid
     #norm_dot_product = 1/(1+np.exp(-norm_dot_product))
-    norm_dot_product = (norm_dot_product + 1) /2
+    
 
 
     # Apply the lighting calculation to positive
@@ -39,9 +42,27 @@ def getLitty(normals, colors, size,lightingVec):
     full_norms[:,1] = norm_dot_product
     full_norms[:,2] = norm_dot_product
 
+    #Camera angle and light reflection off normal calculations for highlights
+    reflection_angles = (-(full_norms*(newNormals/128))*2)+np.tile(lightingVec,(size[0]*size[1],1))
+    bounce_light = np.zeros((size[0]*size[1],3))
+    bounce_light[:,0] = (np.dot(-reflection_angles,np.array(CAMERA_ANGLE/norm(CAMERA_ANGLE))) + 1) /2
+    bounce_light[:,1] = bounce_light[:,0]
+    bounce_light[:,2] = bounce_light[:,0]
+    norm_dot_product = (norm_dot_product + 1) /2
+    full_norms[:,0] = norm_dot_product
+    full_norms[:,1] = norm_dot_product
+    full_norms[:,2] = norm_dot_product
+
+    highlights = (bounce_light[:,0] > 1)
+
     color_adjust_dot = np.zeros((size[0]*size[1],3))
 
-    color_adjust_dot=(DARK_COLOR)-full_norms*((DARK_COLOR)-(colors_rgb))
+    #adding darkness when sun angle is low
+    color_adjust_dot=((DARK_COLOR)-full_norms*((DARK_COLOR)-(colors_rgb)))
+
+    #adding highlights when bounce light is similar to camera angle
+    #color_adjust_dot[highlights] = color_adjust_dot[highlights]*(1-bounce_light[highlights]) + LIGHT_COLOR*(bounce_light[highlights])
+    color_adjust_dot[highlights] = ((color_adjust_dot[highlights] + LIGHT_COLOR*(1))/2)
     colors_rgb[maskNotVisible] = 0
     color_adjust_dot.reshape((size[0],size[1],3))
     
@@ -65,4 +86,5 @@ def getLitty(normals, colors, size,lightingVec):
     return pygame.image.fromstring(bytes(np.array(newColors, dtype=np.uint8).reshape(((size[0]*size[1]))*4)), size, 'RGBA')
 
 def getLightingVecXZ(z):
-    return np.array([math.cos(z/(18*DAYLIGHT_DIVISOR)-math.pi*4/9),2*min(math.sin(z/(18*DAYLIGHT_DIVISOR)-math.pi/4),0),math.sin(z/(18*DAYLIGHT_DIVISOR)-math.pi*4/9)])
+    return np.array([math.cos(z/(18*DAYLIGHT_DIVISOR)-math.pi*4/9),6*min(math.sin(z/(18*DAYLIGHT_DIVISOR)-math.pi/4),0),math.sin(z/(18*DAYLIGHT_DIVISOR)-math.pi*4/9)])
+    #return np.array([-math.sin(z/(18*DAYLIGHT_DIVISOR)),2*min(math.sin((z-(7.5*math.pi/2))/(18*DAYLIGHT_DIVISOR)-math.pi/4),0),math.cos(z/(18*DAYLIGHT_DIVISOR))])
